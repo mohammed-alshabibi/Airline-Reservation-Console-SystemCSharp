be2d25351785a670dd049f0e6e3c1b242ef4c30e
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-04-10 10:50:42 +0400

    create showmain menu function

--- Program.cs
+++ Program.cs
@@ -5,10 +5,36 @@
         static void Main(string[] args)
         {
             DisplayWelcomeMessage();
+            ShowMainMenu();
         }
         public static void DisplayWelcomeMessage()
         {
             Console.WriteLine("Welcome To Airline Reservation System");
+        }
+        public static int ShowMainMenu()
+        {
+            Console.WriteLine("Select Options:");
+            Console.WriteLine("1. Book Flight");
+            Console.WriteLine("2. Cancel");
+            Console.WriteLine("3. View Flights");
+            Console.WriteLine("4. Exit");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
+            return choice;
+
         }
     }
 
