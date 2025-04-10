e1a809f0133daafaa0494e8e3edca5f6bc4a1756
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-04-10 10:37:32 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Airline_Reservation_Console_SystemCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
